5c84970609363e125ca7cf6dda79b78a80ff389c
asemelkhouli20 <asemelkhouli209@gmail.com>
2024-06-02 15:57:51 +0300

    last

--- JS/script.js
+++ JS/script.js
@@ -65,3 +65,15 @@ setHead("activeHome");
 setFooter();
 setFeatures();
 setService();
+
+ var Tawk_API = Tawk_API || {},
+   Tawk_LoadStart = new Date();
+ (function () {
+   var s1 = document.createElement("script"),
+     s0 = document.getElementsByTagName("script")[0];
+   s1.async = true;
+   s1.src = "https://embed.tawk.to/665c68259a809f19fb37eb12/1hvcfdlcc";
+   s1.charset = "UTF-8";
+   s1.setAttribute("crossorigin", "*");
+   s0.parentNode.insertBefore(s1, s0);
+ })();
